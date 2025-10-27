f520a06e239879e461ebb82f2b33e7bcb3d8633c
Elena Parapounsky <eparapounsky@gmail.com>
2025-10-27 11:46:17 -0700

    Create file with webpage url

--- src/Download.java
+++ src/Download.java
@@ -1,24 +1,58 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+
+import java.util.Set;
+import java.io.File;
+import java.io.IOException;
 
 public class Download {
-
-    public static void main(String[] args) {
+    static void createFile(Document webpage) {
+        // create a file to store the downloaded content
         try {
-            Document doc = Jsoup.connect("https://www.elenaparapounsky.com/").get(); // download the page HTML
-            //System.out.println("Title: " + doc.title());
+            String file_name = webpage.location().replace("https://www.", ""); // remove invalid file name syntax
 
-            Elements links = doc.select("a"); // extract all links
+            System.out.println(file_name);
+            File page_file = new File(file_name); // create file object
 
-            // for each loop
-            for (Element link : links) {
-                System.out.printf("%s\n\t%s\n",
-                        link.attr("link"), link.absUrl("href"));
+            if (page_file.createNewFile()) {           // try to create the file
+                System.out.println("File created: " + page_file.getName());
+            } else {
+                System.out.println("File already exists.");
             }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    static void crawl() {
+        try {
+            Document webpage = Jsoup.connect("https://www.elenaparapounsky.com/").get(); // download the page HTML
+            createFile(webpage);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    public static void main(String[] args) {
+        crawl();
+    }
+
+
+//    public static void main(String[] args) {
+//        try {
+//            Document webpage = Jsoup.connect("https://www.elenaparapounsky.com/").get(); // download the page HTML
+//            //System.out.println("Title: " + webpage.title());
+//
+//            Elements links = webpage.select("a"); // extract all links
+//
+//            // for each loop
+//            for (Element link : links) {
+//                System.out.printf("%s\n", link.absUrl("href"));
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
