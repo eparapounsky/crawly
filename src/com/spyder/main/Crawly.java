8b79d2c3d0784f53f129dbd7535c993216f83381
Elena Parapounsky <eparapounsky@gmail.com>
2025-11-01 11:01:12 -0700

    Rename Spyder to Crawly

--- src/com/spyder/main/Crawly.java
+++ src/com/spyder/main/Crawly.java
@@ -1,6 +1,6 @@
 package com.spyder.main;
 
-public class Spyder {
+public class Crawly {
 
     public static void main(String[] args) {
         new Downloader("https://www.elenaparapounsky.com/", "./output").crawl();
