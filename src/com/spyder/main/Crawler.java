7cdd60472d9e5bbafde342e5d8615d168e77a409
Elena Parapounsky <eparapounsky@gmail.com>
2025-11-01 11:31:55 -0700

    Add Crawler class for web crawling functionality

---
+++ src/com/spyder/main/Crawler.java
@@ -0,0 +1,26 @@
+package com.spyder.main;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class Crawler {
+
+    private String url;
+    private String saveLocation;
+    private Downloader downloader;
+
+    public Crawler(String url, String saveLocation) {
+        this.url = url;
+        this.saveLocation = saveLocation;
+        this.downloader = downloader;
+    }
+
+    public void crawl() {
+        try {
+            Document webpage = Jsoup.connect(this.url).get(); // store parsed html
+            downloader.download(webpage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
