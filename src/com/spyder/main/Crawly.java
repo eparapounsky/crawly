c779920621d7e655b4fbea44b70a280550881c40
Elena Parapounsky <eparapounsky@gmail.com>
2025-11-01 19:08:39 -0700

    Remove null argument

--- src/com/spyder/main/Crawly.java
+++ src/com/spyder/main/Crawly.java
@@ -5,7 +5,7 @@ public class Crawly {
     public static void main(String[] args) {
         String url = "https://www.elenaparapounsky.com/";
         String saveLocation = "./output";
-        Downloader downloader = new Downloader(null); // create the dependency
+        Downloader downloader = new Downloader(); // create the dependency
         Crawler crawler = new Crawler(url, saveLocation, downloader); // inject dependency
         crawler.crawl();
     }
