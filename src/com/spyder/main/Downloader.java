1d75b2f5af7305dfe461a297a09dc2bbd0274305
Elena Parapounsky <eparapounsky@gmail.com>
2025-11-01 19:06:52 -0700

    Remove unnecessary constructor and instance variable

--- src/com/spyder/main/Downloader.java
+++ src/com/spyder/main/Downloader.java
@@ -7,22 +7,14 @@ import java.io.IOException;
 
 public class Downloader {
 
-    private Document webpage;
-
-    public Downloader(Document webpage) {
-        this.webpage = webpage;
-    }
-
     public void download(Document webpage) {
         createFile(webpage);
     }
 
     private void createFile(Document webpage) {
-        // create a file to store the downloaded content
+
         try {
             String file_name = webpage.location().replace("https://www.", ""); // remove invalid file name syntax
-
-            System.out.println(file_name);
             File page_file = new File(file_name); // create file object
 
             if (page_file.createNewFile()) {           // try to create the file
