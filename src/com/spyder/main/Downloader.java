0078f31603b338e1d16c4ee1452783276a9627ae
Elena Parapounsky <eparapounsky@gmail.com>
2025-11-02 10:47:56 -0800

    Simplify file creation with FileWriter

--- src/com/spyder/main/Downloader.java
+++ src/com/spyder/main/Downloader.java
@@ -2,28 +2,16 @@ package com.spyder.main;
 
 import org.jsoup.nodes.Document;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.FileWriter;
 
 public class Downloader {
 
     public void download(Document webpage) {
-        createFile(webpage);
-    }
-
-    private void createFile(Document webpage) {
-
         try {
-            String file_name = webpage.location().replace("https://www.", ""); // remove invalid file name syntax
-            File page_file = new File(file_name); // create file object
-
-            if (page_file.createNewFile()) {           // try to create the file
-                System.out.println("File created: " + page_file.getName());
-            } else {
-                System.out.println("File already exists.");
-            }
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
+            FileWriter myWriter = new FileWriter(webpage.title());
+            myWriter.write("File test");
+            myWriter.close();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
