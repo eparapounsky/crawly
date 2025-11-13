6b568698cef2d77f54ff283b83dab8e04b8b500a
Elena Parapounsky <eparapounsky@gmail.com>
2025-11-12 17:46:00 -0800

    Add directory creation in Downloader to ensure saveLocation exists

--- src/main/java/com/spyder/main/Downloader.java
+++ src/main/java/com/spyder/main/Downloader.java
@@ -2,9 +2,14 @@ package com.spyder.main;
 
 import org.jsoup.nodes.Document;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class Downloader {
+
     private final String saveLocation;
 
     public Downloader(String saveLocation) {
@@ -12,6 +17,14 @@ public class Downloader {
     }
 
     public void download(Document webpage) {
+        // create output directory if it doesn't exist
+        try {
+            Path filepath = Paths.get(saveLocation);
+            Files.createDirectories(filepath);
+        } catch (IOException e) {
+            System.err.println("Error creating nested directories: " + e.getMessage());
+        }
+
         String filename = webpage.title().replace(".", "");
 
         try (FileWriter myWriter = new FileWriter(filename + ".html")) {
