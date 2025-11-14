f2127a8243f8d95abbe532ae8b8b7de5c1d84c0f
Elena Parapounsky <eparapounsky@gmail.com>
2025-11-13 18:59:05 -0800

    Change url for testing

--- src/main/java/com/spyder/main/Crawly.java
+++ src/main/java/com/spyder/main/Crawly.java
@@ -3,8 +3,8 @@ package com.spyder.main;
 public class Crawly {
 
     public static void main(String[] args) {
-        String url = "https://books.toscrape.com/";
-        // String url = "C:\\git\\Crawly\\output\\index.html";
+        String url = "https://toscrape.com/";
+        // String url = "C:\\git\\Crawly\\input\\site1\\index.html";
         String saveLocation = "./output";
         Downloader downloader = new Downloader(saveLocation); // create the dependency
         Crawler crawler = new Crawler(url, downloader, saveLocation); // inject dependency
