309fd4ca07405351f5b20af803bcf97eb4f263f1
Elena Parapounsky <eparapounsky@gmail.com>
2025-11-16 17:29:05 -0800

    Remove commented-out local URL (not needed for testing)

--- src/main/java/com/spyder/main/Crawly.java
+++ src/main/java/com/spyder/main/Crawly.java
@@ -4,7 +4,6 @@ public class Crawly {
 
     public static void main(String[] args) {
         String url = "https://toscrape.com/";
-        // String url = "C:\\git\\Crawly\\input\\site1\\index.html";
         String saveLocation = "./output";
         Downloader downloader = new Downloader(saveLocation); // create the dependency
         Crawler crawler = new Crawler(url, downloader); // inject dependency
