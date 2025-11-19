d15402adeea6cc3ac322f14c081f9b109b011c29
Elena Parapounsky <eparapounsky@gmail.com>
2025-11-18 18:16:07 -0800

    Use System.Logger instead of utils.logger

--- src/main/java/com/spyder/main/Crawly.java
+++ src/main/java/com/spyder/main/Crawly.java
@@ -1,11 +1,11 @@
 package com.spyder.main;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.lang.System.Logger;
+import java.lang.System.Logger.Level;
 
 public class Crawly {
 
-    private static final Logger logger = Logger.getLogger(Crawly.class.getName());
+    private static final Logger logger = System.getLogger(Crawly.class.getName());
 
     public static void main(String[] args) {
         logger.log(Level.INFO, "Started Crawly");
@@ -14,15 +14,15 @@ public class Crawly {
         String saveLocation = "./output";
 
         logger.log(Level.INFO, "Initializing Crawly with URL: {0} and Save Location: {1}",
-                new Object[] { url, saveLocation });
+                url, saveLocation);
 
         try {
             Downloader downloader = new Downloader(saveLocation); // create the dependency
             Crawler crawler = new Crawler(url, downloader); // inject dependency
             crawler.crawl();
         } catch (Exception e) {
-            logger.log(Level.SEVERE, "Error occurred during crawling: {0}", e.getMessage());
-            logger.log(Level.SEVERE, "Stack trace:", e);
+            logger.log(Level.ERROR, "Error occurred during crawling: {0}", e.getMessage());
+            logger.log(Level.ERROR, "Stack trace:", e);
         }
 
         logger.log(Level.INFO, "Crawly finished");
