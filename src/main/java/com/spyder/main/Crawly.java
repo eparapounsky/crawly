78fbe480d3b58ae72bead366b767a896f7a451c3
Elena Parapounsky <eparapounsky@gmail.com>
2025-11-19 18:24:55 -0800

    Use Scanner to get user input for URL

--- src/main/java/com/spyder/main/Crawly.java
+++ src/main/java/com/spyder/main/Crawly.java
@@ -2,15 +2,19 @@ package com.spyder.main;
 
 import java.lang.System.Logger;
 import java.lang.System.Logger.Level;
+import java.util.Scanner;
 
 public class Crawly {
 
     private static final Logger logger = System.getLogger(Crawly.class.getName());
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         logger.log(Level.INFO, "Started Crawly");
 
-        String url = "https://toscrape.com/";
+        // String url = "https://toscrape.com/";
+        String url = getUserInput();
+        System.out.println(url);
         String saveLocation = "./output";
 
         logger.log(Level.INFO, "Initializing Crawly with URL: {0} and Save Location: {1}",
@@ -26,5 +30,11 @@ public class Crawly {
         }
 
         logger.log(Level.INFO, "Crawly finished");
+    }
+
+    private static String getUserInput() {
+        System.out.print("Enter website: ");
+        String website = scanner.nextLine();
+        return website;
     }
 }
